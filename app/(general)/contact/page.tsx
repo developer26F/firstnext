ad92087f158834e1b07c1ffcbef7fed5b0173b5f
Oanguiano28 <developer8@grupotransforma.mx>
2023-12-22 20:11:24 -0600

    first commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+ title: 'SEO Contact',
+ description: 'SEO Contact Description',
+};
+
+export default function ContactPage(){
+    return (
+        <>
+        <span className="text-7xl">ContactPage</span>
+        </>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+ title: 'SEO Title Pricing ',
+ description: 'SEO Title Pricing Description',
+};
+
+export default function PricingPage(){
+    return (
+        <>
+        <span className="text-7xl">PricingPage</span>
+        </>
+    )
+}
